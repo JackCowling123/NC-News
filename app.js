5497032e172b346ab0d7feebd870c90aec179ffa
JackCowling123 <jackcowling77@gmail.com>
2024-05-29 22:03:38 +0100

    This commit added the ability to return all articles through api/articles request. Appropriate testing also added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const {getTopics, getAPI, getArticlesID} = require('./controllers/controllers.js')
+const {getTopics, getAPI, getArticlesID, getAllArticles} = require('./controllers/controllers.js')
 
 app.get('/api/topics', getTopics);
 app.get('/api', getAPI);
 app.get('/api/articles/:article_id', getArticlesID);
+app.get('/api/articles', getAllArticles);
 
 
 // Middleware to catch 404 errors (route not found)
@@ -17,7 +18,6 @@ app.use((req, res, next) => {
 
 // Middleware to handle specific Postgres error codes
 app.use((err, req, res, next) => {
-    console.log(err);
     if (err.code === '22P02' || err.code === '23502') {
         res.status(400).send({ msg: 'Bad request' });
     } else {
@@ -32,6 +32,10 @@ app.use((err, req, res, next) => {
     } else {
         next(err);
     }
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: 'Internal Server Error' });
 });
 
 
--- models/models.js
+++ models/models.js
@@ -19,3 +19,12 @@ exports.selectArticleByID = (article_id) => {
             }
         })
 }
+
+exports.selectAllArticles = () => {
+    console.log('models');
+    return db
+        .query('SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;')
+        .then((result) => {
+            return result.rows;
+        })
+}
