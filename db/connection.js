15e610b3dab6022399dbf60e0ea4f2510bbe62bb
JackCowling123 <jackcowling77@gmail.com>
2024-06-11 21:33:07 +0100

    minor changes ti cinnection.js

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,23 @@
 const { Pool } = require('pg');
-const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
-const config = {};
+const config = {}
 
-require('dotenv').config({
-  path: path.resolve(__dirname, `../.env.${ENV}`)
-});
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;  // Adjust max connections if needed
-} else {
-  config.user = process.env.PGUSER;
-  config.host = process.env.PGHOST;
-  config.database = process.env.PGDATABASE;
-  config.password = process.env.PGPASSWORD;
-  config.port = process.env.PGPORT;
-
-  if (!config.database) {
-    throw new Error('PGDATABASE not set in development environment');
-  }
+  config.max = 2;
 }
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set');
+}
+
+
 
 module.exports = new Pool(config);
