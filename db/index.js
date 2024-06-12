77a82f290bb074513c23e0a7541b6175a05fce66
JackCowling123 <jackcowling77@gmail.com>
2024-06-12 14:31:00 +0100

    changes made to the path in index.js

--- db/index.js
+++ db/index.js
@@ -4,6 +4,8 @@ const ENV = process.env.NODE_ENV || "development";
 
 const path = `${__dirname}/../.env.${ENV}`;
 
+console.log(path);
+
 require("dotenv").config({ path });
 
 if (!process.env.PGDATABASE) {
